85e04c7014b0b379fec286644f5f0ff588d89637
chaeyeon <321codus@naver.com>
2023-05-21 02:24:24 +0900

    shooting 구현

---
+++ Assets/Scripts/Shooting.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+    [SerializeField] private GameObject firePos;  //총알 생성 위치
+    [SerializeField] private GameObject hitFx; //총알 맞았다는 효과  
+    private Transform swanPoint; //ray 선 끝 위치
+    Color rayColor = Color.red; //ray 선 색깔
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        DrawRay();
+        //trigger 누를 때
+        if (OVRInput.GetDown(OVRInput.Button.SecondaryIndexTrigger))
+        {
+            TriggerShoot();
+        }
+    }
+
+    public void TriggerShoot()
+    {
+        Ray ray = new Ray(firePos.transform.position, firePos.transform.forward);
+        RaycastHit hitInfo;
+        //크리스털에 총알이 맞았으면
+        if(Physics.Raycast(ray, out hitInfo))
+        {
+            if(hitInfo.collider.tag == "crystal")
+            {
+                BulletImpact(hitInfo);
+                Destroy(hitInfo.transform.gameObject);
+                //크리스털 깨질 때 효과 넣을 거면 함수 생성해서 넣기
+            }
+        }
+
+    }
+
+    public void BulletImpact(RaycastHit hitInfo)
+    {
+        ParticleSystem ps = hitFx.GetComponent<ParticleSystem>();
+        ps.Play();  //효과 생성
+        //Instantiate(hitFx, hitInfo.point, Quaternion.identity); 
+    }
+    
+    void DrawRay()
+    {
+        Debug.DrawRay(firePos.transform.position, swanPoint.forward * 30, rayColor);
+    }
+}
